591abfde1c4d28c4a0c31cf04331d6eb3569779e
Virallium <mengimiradit@gmail.com>
2026-08-11 02:27:41 +0200

    deploiement sur render de l'app

---
+++ staticfiles/js/style.js
@@ -0,0 +1,72 @@
+function AOS_anim(){
+    
+        document.addEventListener('DOMContentLoaded', function() {
+            // Initialisation d'AOS
+            AOS.init({
+                duration: 800,
+                once: false,
+                mirror: true
+            });
+
+            // Initialisation de Swiper
+            const swiper = new Swiper('.mySwiper', {
+                effect: 'coverflow',
+                grabCursor: true,
+                centeredSlides: true,
+                slidesPerView: 'auto',
+                coverflowEffect: {
+                    rotate: 30,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            });
+        });
+}
+AOS_anim();
+
+
+function menu_toggle(){
+    const menu_bar = document.querySelector('#menu');
+        const nav = document.querySelector('nav');
+        const rgba = document.querySelector('.rgba');
+
+        if (menu_bar && nav && rgba) {
+            menu_bar.addEventListener('click', () => {
+                nav.classList.toggle('active');
+                rgba.classList.toggle('active');
+            });
+
+            rgba.addEventListener('click', () => {
+                nav.classList.remove('active');
+                rgba.classList.remove('active');
+            });
+
+            document.querySelectorAll('nav a').forEach(link => {
+                link.addEventListener('click', () => {
+                    nav.classList.remove('active');
+                    rgba.classList.remove('active');
+                });
+            });
+        }
+}
+menu_toggle();
+
+function scroll_nav(){
+    const nav = document.querySelector('nav');
+    const scrollThreshold = 150;
+    window.addEventListener('scroll', ()=>{
+        if (window.scrollY > scrollThreshold){
+            nav.classList.add('scrolled')
+        }
+        else{
+            nav.classList.remove('scrolled')
+        }
+    })
+}
+scroll_nav();
